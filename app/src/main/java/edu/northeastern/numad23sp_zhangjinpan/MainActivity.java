5b6b7dd5fa0b3b226febc86497e340ee4df6cdcf
Zhangjin-666 <Patrick1998pzj@gmail.com>
2023-01-26 18:01:26 -0800

    a1_zhangjinPan

--- app/src/main/java/edu/northeastern/numad23sp_zhangjinpan/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_zhangjinpan/MainActivity.java
@@ -3,6 +3,9 @@ package edu.northeastern.numad23sp_zhangjinpan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_1 = (Button) findViewById(R.id.button_1);
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "ZhangjinPan(pan.zhan@northeastern.edu)",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        Button button_2 = (Button) findViewById(R.id.button_2);
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Yes! Of course!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
